bb0075a9bb030ab91a8a4fc41cb710a986cc4c1b
walidbee <walid.boudaoud@gmail.com>
2021-02-03 12:14:20 +0100

    Updated NMF.py

--- NMF.py
+++ NMF.py
@@ -1,101 +1,66 @@
 from movieLensDataset import *
 import numpy as np
+from numpy.linalg import lstsq
 
-class MF():
+class NMF():
 
-    def __init__(self, R, K, alpha, beta, iterations):
+    def __init__(self, M, K, steps, beta):
         """
-        Perform matrix factorization to predict empty
-        entries in a matrix.
+        Non-negative matrix factorization using ALS 
 
         Arguments
-        - R (ndarray)   : user-item rating matrix
-        - K (int)       : number of latent dimensions
-        - alpha (float) : learning rate
-        - beta (float)  : regularization parameter
+        - M (matrix)   : rating matrix
+        - K (int)       : number of latent features
+        - steps (int) : number of times we run the algorithm
+        - beta (float) : added number so we don't divide by 0 (used only for Multiplication Update algorithm)
         """
 
-        self.R = R
-        self.num_users, self.num_items = R.shape
+        self.M = M
+        self.X, self.Y = M.shape
         self.K = K
-        self.alpha = alpha
+        self.steps = steps
         self.beta = beta
-        self.iterations = iterations
 
-    def train(self):
-        # Initialize user and item latent feature matrice
-        self.P = np.random.normal(scale=1./self.K, size=(self.num_users, self.K))
-        self.Q = np.random.normal(scale=1./self.K, size=(self.num_items, self.K))
+    def computeALS(self):
+        # Alternating Least Squares
+        # M = PQ
+        #initialize P and Q matrices
+        P = np.random.rand(self.X, self.K)
+        Q = np.random.rand(self.K, self.Y)
+    
+        for _ in range(self.steps):
+            #resolve P @ Q = M
+            Q = lstsq(P, self.M, rcond = -1)[0]
+            #negative terms = 0
+            Q[Q < 0] = 0
 
-        # Initialize the biases
-        self.b_u = np.zeros(self.num_users)
-        self.b_i = np.zeros(self.num_items)
-        self.b = np.mean(self.R[np.where(self.R != 0)])
+            #resolve Q.T @ P = M.T
+            P = lstsq(Q.T, self.M.T, rcond = -1)[0].T
+            #negative terms = 0
+            P[P < 0] = 0
 
-        # Create a list of training samples
-        self.samples = [
-            (i, j, self.R[i, j])
-            for i in range(self.num_users)
-            for j in range(self.num_items)
-            if self.R[i, j] > 0
-        ]
+        return (P @ Q).round(decimals=1)
 
-        # Perform stochastic gradient descent for number of iterations
-        training_process = []
-        for i in range(self.iterations):
-            np.random.shuffle(self.samples)
-            self.sgd()
-            mse = self.mse()
-            training_process.append((i, mse))
-            if (i+1) % 10 == 0:
-                print("Iteration: %d ; error = %.4f" % (i+1, mse))
+    def computeMU(self):
+        # Multiplication Update
+        # M = PQ
+        #initialize P and Q matrices
+        P = np.random.rand(self.X, self.K)
+        Q = np.random.rand(self.K, self.Y)
 
-        return training_process
+        for _ in range(self.steps):
 
-    def mse(self):
-        """
-        A function to compute the total mean square error
-        """
-        xs, ys = self.R.nonzero()
-        predicted = self.full_matrix()
-        error = 0
-        for x, y in zip(xs, ys):
-            error += pow(self.R[x, y] - predicted[x, y], 2)
-        return np.sqrt(error)
+            Q = Q * (P.T.dot(self.M) / P.T.dot(P).dot(Q) + self.beta)
 
-    def sgd(self):
-        """
-        Perform stochastic graident descent
-        """
-        for i, j, r in self.samples:
-            # Computer prediction and error
-            prediction = self.get_rating(i, j)
-            e = (r - prediction)
+            P = P * (self.M.dot(Q.T) / P.dot(Q).dot(Q.T) + self.beta)
 
-            # Update biases
-            self.b_u[i] += self.alpha * (e - self.beta * self.b_u[i])
-            self.b_i[j] += self.alpha * (e - self.beta * self.b_i[j])
-
-            # Update user and item latent feature matrices
-            self.P[i, :] += self.alpha * (e * self.Q[j, :] - self.beta * self.P[i,:])
-            self.Q[j, :] += self.alpha * (e * self.P[i, :] - self.beta * self.Q[j,:])
-
-    def get_rating(self, i, j):
-        """
-        Get the predicted rating of user i and item j
-        """
-        prediction = self.b + self.b_u[i] + self.b_i[j] + self.P[i, :].dot(self.Q[j, :].T)
-        return prediction
-
-    def full_matrix(self):
-        """
-        Computer the full matrix using the resultant biases, P and Q
-        """
-        return self.b + self.b_u[:,np.newaxis] + self.b_i[np.newaxis:,] + self.P.dot(self.Q.T)
+        return (P @ Q).round(decimals=1)
 
 rec_matrix = ratingsMatrix()
 
-mf = MF(rec_matrix, K=2, alpha=0.1, beta=0.01, iterations=20)
+NMF = NMF(rec_matrix, K=2, steps=20, beta= 0.002)
 
-mf.train()
+result = NMF.computeMU()
+
+print(result)
 
